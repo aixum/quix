a2b7fa2a633901017673d179ca7124a72a91ce53
Matin Moharami <matinmoharami@yahoo.com>
2024-10-17 20:37:58 +0330

    initial commit

---
+++ quix/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from quix.users.models import User
+from quix.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
